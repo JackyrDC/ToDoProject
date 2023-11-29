2758983742d224b54c3b12fccbc9b0b34e94060b
JackyrDC <djackyrcalix@gmail.com>
2023-11-29 08:52:37 -0600

    fix: 🐛 Fixed colors definition on taillwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
- import { colors as color }  from 'tailwindcss/colors';
+import { colors as color } from 'tailwindcss/colors';
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      blue: color.blue,
-      gray: color.gray,
-      
-    },
-    extend: {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+
+        },
+        extend: {
 
 
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
